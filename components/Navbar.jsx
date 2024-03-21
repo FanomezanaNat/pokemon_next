001262a79d749ca1dea2176254daaa0d756fa69e
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-03-22 01:36:17 +0300

    feat: add client component

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,22 +3,22 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav class="w-screen bg-gray-950 h-fit overflow-hidden flex flex-row justify-center">
-      <div class="py-4 lg:px-8 px-4 max-w-[1280px] h-16 m-auto text-white flex items-center justify-between">
+    <nav className="w-screen bg-gray-950 h-fit overflow-hidden flex flex-row justify-center">
+      <div className="py-4 lg:px-8 px-4 max-w-[1280px] h-16 m-auto text-white flex items-center justify-between">
         <div
-          class="flex lg:gap-8 gap-6  tracking-wider cursor-pointer text-lg items-center"
+          className="flex lg:gap-8 gap-6  tracking-wider cursor-pointer text-lg items-center"
           id="navItems"
         >
           <Link href="/server">
-            <span class="group">
+            <span className="group">
               Server
-              <div class="w-0 group-hover:w-full h-0.5 bg-white ease-in-out duration-500"></div>
+              <div className="w-0 group-hover:w-full h-0.5 bg-white ease-in-out duration-500"></div>
             </span>
           </Link>
             <Link href="/client">
-            <span class="group">
+            <span className="group">
               Client
-              <div class="w-0 group-hover:w-full h-0.5 bg-white ease-in-out duration-500"></div>
+              <div className="w-0 group-hover:w-full h-0.5 bg-white ease-in-out duration-500"></div>
             </span>
           </Link>
         </div>
