2890eb1b2fcf84691927228fe5ed7651a24c2659
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-03-22 09:54:29 +0300

    fix: update

--- app/client/page.jsx
+++ app/client/page.jsx
@@ -1,17 +1,17 @@
 "use client";
 import Link from "next/link";
-import usePokemonData from "@/components/hooks/UsePokemonData";
+import UsePokemonDetails from "@/components/hooks/UsePokemonDetails";
 
 export default function page() {
-  const { pokemonDetails,pokemonList } = usePokemonData();
+  const { pokemonDetailsUrl,pokemonList } = UsePokemonDetails();
   return (
     <div className="container mx-auto">
       <h1 className="text-3xl font-bold mb-8">Pokemon List</h1>
       <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {pokemonDetails.map((pokemon, id) => (
+        {pokemonDetailsUrl.map((pokemon, id) => (
           <li key={id} className="bg-grey shadow-md rounded-lg overflow-hidden">
             <img
-              src={`${pokemon}`}
+              src={`${pokemon.sprites.front_default}`}
               alt={`pokemon ${id + 1}`}
               className="w-full"
             />
--- app/server/page.jsx
+++ app/server/page.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import Link from "next/link";
+import FetchDetails from "@/components/function/FetchDetails";
 
 export default async function page() {
-  const data = await fetch(
-    "https://pokeapi.co/api/v2/pokemon?limit=50&offset=0"
-  );
-  const res = await data.json();
+  const details= await FetchDetails();
+ 
   return (
     <div className="container mx-auto">
       <h1 className="text-3xl font-bold mb-8">Pokemon List</h1>
       <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {res.results.map((pokemon, index) => (
+        {details.results.map((pokemon, index) => (
           <li
             key={index}
             className="bg-grey shadow-md rounded-lg overflow-hidden"
@@ -24,7 +23,7 @@ export default async function page() {
             />
             <div className="p-4 flex flex-col items-center">
               <p className="text-xl font-semibold mb-2 text-center">{pokemon.name}</p>
-              <Link href={`/server`}>
+              <Link href={`/server/${index+1}`}>
                 <button class="btn overflow-hidden relative w-64 bg-transparent text-white py-4 px-4 rounded-xl font-bold uppercase -- before:block before:absolute before:h-full before:w-1/2 before:rounded-full before:bg-blue-400 before:top-0 before:left-1/4 before:transition-transform before:opacity-0 before:hover:opacity-100 hover:text-white-200 hover:before:animate-ping transition-all duration-300">
                   <span class="relative">details</span>
                 </button>
